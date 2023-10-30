2c40a93b4effc59c799efbd2a4f21cf6cdcf34dd
Rishabh Singh <rishabh15112001@gmail.com>
2023-10-31 03:15:36 +0530

    important change

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -6,7 +6,7 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
+          "connect-src": ["'self'", "http:", "https:"],
           "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
           "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
           upgradeInsecureRequests: null,
